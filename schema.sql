c7aae1c096d1b0df931697e06381b0b190c12608
Alexandra Luna <alexandra.michelle.j@gmail.com>
2019-12-15 15:00:26 -0800

    created sql tables and inserted data into them

---
+++ schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    DepartmentID INTEGER AUTO_INCREMENT NOT NULL,
+    dep_name VARCHAR(30),
+    PRIMARY KEY(DepartmentID)
+);
+
+CREATE TABLE roles (
+    RoleID INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    DepartmentID INTEGER,
+
+    PRIMARY KEY(RoleID),
+    FOREIGN KEY(DepartmentID) REFERENCES department(DepartmentID)
+
+);
+
+CREATE TABLE employee(
+    EmployeeID INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    RoleID INTEGER,
+    ManagerID INTEGER NULL,
+
+    PRIMARY KEY(EmployeeID),
+    FOREIGN KEY(RoleID) REFERENCES roles(RoleID),
+    FOREIGN KEY(ManagerID) REFERENCES employee(EmployeeID)
+
+
+);
+
+
+
+
---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE employee_trackerDB;
+
+INSERT INTO department (dep_name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO roles (title, salary, DepartmentID)
+VALUES ("Sales Lead", 100000, 1),("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 70000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, RoleID)
+VALUE ("Alexandra", "Jackson", 5), ("Jennifer", "Carrier", 1), ("Eric", "Thompson", 7), ("Sam", "Trice", 4), ("Sophie", "David", 3), ("Bob", "Dylan", 2), ("Carrie", "Donald", 6), ("Ken", "Dude", 3);
