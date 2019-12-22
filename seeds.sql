0a23e480d05d5767736d43b1a4e662299cc2fff4
Alexandra Luna <alexandra.michelle.j@gmail.com>
2019-12-21 21:57:13 -0800

    small change to seed file

--- seeds.sql
+++ seeds.sql
@@ -6,7 +6,7 @@ VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 INSERT INTO roles (title, salary, DepartmentID)
 VALUES ("Sales Lead", 100000, 1),("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 70000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, RoleID)
-VALUE ("Alexandra", "Jackson", 5), ("Jennifer", "Carrier", 1), ("Eric", "Thompson", 7), ("Sam", "Trice", 4), ("Sophie", "David", 3), ("Bob", "Dylan", 2), ("Carrie", "Donald", 6), ("Ken", "Dude", 3);
+INSERT INTO employee (first_name, last_name, RoleID, ManagerID)
+VALUE ("Alexandra", "Jackson", 5, 2), ("Jennifer", "Carrier", 1, 2), ("Eric", "Thompson", 7, 3), ("Sam", "Trice", 4, 1), ("Sophie", "David", 3, 1), ("Bob", "Dylan", 2, 3), ("Carrie", "Donald", 6, 3), ("Ken", "Dude", 3, 1);
 
 
